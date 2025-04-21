377e5f0ec2c2c3b7efa972c7915a2a29688d7e9d
BlueBeardone <barend.army@gmail.com>
2025-04-21 10:45:06 +0200

    Added inline styling

--- Source/App.py
+++ Source/App.py
@@ -10,10 +10,16 @@ app = dash.Dash(__name__)
 
 # Define Website layout in html
 app.layout = html.Div([
-    html.H1("BrightPath Academy: Student Performance Predictor"),
+    html.H1("BrightPath Academy: Student Performance Predictor",
+    style={'textAlign': 'center',
+            'color': '#2c3e50',
+            'fontFamily': 'Arial',
+            'padding': '20px',
+            'backgroundColor': 'Green'}),
+        
     html.Div([
-        html.Label("Weekly Study Time (hours)"),
-        dcc.Input(id='study_time', type='number', value=5),
+        html.Label("Weekly Study Time (hours)", style={'margin': '10px', 'fontWeight': 'bold'}),
+        dcc.Input(id='study_time', type='number', value=5, style={'margin': '10px', 'padding': '5px'}),
         
         html.Label("Number of Absences"),
         dcc.Input(id='absences', type='number', value=2),
@@ -25,9 +31,18 @@ app.layout = html.Div([
         dcc.Input(id='parental_support', type='number', value=3),
         
         html.Button('Predict', id='predict-button'),
-    ]),
-    html.Div(id='prediction-output')
-])
+    ],
+    style={
+            'width': '50%',
+            'margin': 'auto',
+            'padding': '20px',
+            'border': '1px solid #ddd',
+            'borderRadius': '10px',
+            'backgroundColor': 'white'
+        }),
+    
+    html.Div(id='prediction-output', style={'textAlign': 'center', 'marginTop': '20px', 'fontSize': '20px'})
+], style={'backgroundColor': '#Green', 'minHeight': '100vh'})
 
 #Checks for change on predict btn then grabs inputs and does function and outputs in prediction-outputs as children
 @app.callback(
